68e08145c636eb35ed136106681bcf58b057cbad
KravAD <dkrav@myuax.com>
2023-11-28 12:18:29 +0100

    commit 9
    Vuelvo el nombre de la enumeración variant_type y le doy las características públicas de clase variant.

--- Ejercicio1.cpp
+++ Ejercicio1.cpp
@@ -5,10 +5,30 @@
 #include "json11.hpp"
 #include "jsonlib.hpp"
 
-enum variant{ simbolo, numero, lista, proc, lambda, cadena};
+enum variant_type{ simbolo, numero, lista, proc, lambda, cadena};
 
 struct entorno;
 
-class variant {
+class Variant {
+    public:
+    typedef Variant(*proc_type) ( const std::vector<Variant>& );
+    typedef std::vector<Variant>::const_iterator iter;
+    typedef std::map<std::string, Variant> map;
+
+    variant_type type;
+    std::string val;
+    std::vector<Variant> list;
+    proc_type proc;
+    entorno* env;
+
+    Variant(variant_type type = simbolo) : type(type) , env(0), proc(0) { }
+    Variant(variant_type type, const std::string& val) : type(type), val(val) , env(0) , proc(0) { }
+    Variant(proc_type proc) : type(proc), proc(proc) , env(0) { }
+
+    std::string to_string();
+    std::string to_json_string();
+    static Variant from_json_string(std::string json);
+    static Variant parse_json(jsonlib::Json job);
 
 };
+
